cfe4676258824c3824cf00c945dcb790766324aa
Snehamayee-Siya <sneha.snehamayee@gmail.com>
2024-06-03 12:45:29 +0530

    First commit...

---
+++ README.md
@@ -0,0 +1,17 @@
+# TicTacToe-Game
+
+## Gameplay
+- The game starts with an empty 3x3 grid.
+- SIngle player and double player.
+- Players take turns to enter their moves.
+- Input validation to ensure proper moves.
+- Scores are monitored.
+- If no player gets three in a row and the grid is full, the game ends in a draw.
+- Clear and user-friendly interface.
+
+## ScreenShots
+![TicTacToe UI](https://github.com/Shivu1126/Android-TicTacToe-Game/assets/99593409/c592ee7a-9d12-436d-8bef-d6407ce2559b)
+## Contributions
+**Contributions to this repository are welcome! If you find any bugs or want to add new features, feel free to open an issue or create a pull request.**
+
+***Have fun playing Tic-Tac-Toe! If you have any questions or suggestions, feel free to contact us at siva11vkavis@gmail.com or shiv1126yt@gmail.com.***
---
+++ app/src/main/java/com/androidwithshiv/tictacteogame/HomeActivity.java
@@ -0,0 +1,90 @@
+package com.androidwithshiv.tictacteogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+
+import com.androidwithshiv.tictacteogame.controller.Common;
+import com.google.android.material.card.MaterialCardView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private MaterialCardView clickX, clickO, singlePlayerButton, duoPlayerButton;
+    private String playerChoose;
+    private Context context;
+    private void init(){
+        clickX = findViewById(R.id.click_x);
+        clickO = findViewById(R.id.click_o);
+        singlePlayerButton = findViewById(R.id.single_player_button);
+        duoPlayerButton = findViewById(R.id.duo_player_button);
+        context = HomeActivity.this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        init();
+
+        Window window = getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(context, R.color.home_bg));
+
+        clickX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerChoose = "x";
+                clickX.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, R.color.light_white_text)));
+                clickO.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, R.color.home_bg)));
+            }
+        });
+
+        clickO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerChoose = "o";
+                clickO.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, R.color.light_white_text)));
+                clickX.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(context, R.color.home_bg)));
+            }
+        });
+
+        singlePlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("single", "clicked");
+                if(playerChoose == null || playerChoose.equals("")){
+                    Common.showToast("Please choose 'X' OR 'O'", HomeActivity.this);
+                }
+                else{
+                    Intent intent = new Intent(HomeActivity.this, GameActivity.class);
+                    intent.putExtra("SingleOrDuo", "single");
+                    intent.putExtra("playerChoose", playerChoose);
+                    startActivity(intent);
+                }
+            }
+        });
+        duoPlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("duo", "clicked");
+                Log.d("duo", "clicked");
+                if(playerChoose == null || playerChoose.equals("")){
+                    Common.showToast("Please choose 'X' OR 'O'", HomeActivity.this);
+                }
+                else{
+                    Intent intent = new Intent(HomeActivity.this, GameActivity.class);
+                    intent.putExtra("SingleOrDuo", "duo");
+                    intent.putExtra("playerChoose", playerChoose);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+}
